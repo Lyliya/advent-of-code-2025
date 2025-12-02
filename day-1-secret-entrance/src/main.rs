89bb77bd3633e60ae40a4bf636315efd40c656a0
Guillaume Guerin <guerin.dev@gmail.com>
2025-12-02 17:44:40 +0100

    chore: use timer create

--- day-1-secret-entrance/src/main.rs
+++ day-1-secret-entrance/src/main.rs
@@ -1,8 +1,9 @@
 use core::str;
 use std::io;
 use std::io::Read;
+use timer;
 
-fn step1(lines: Vec<&str>) {
+fn step1(lines: Vec<&str>) -> usize {
     let mut answer = 0;
     let mut pos = 50;
 
@@ -11,18 +12,27 @@ fn step1(lines: Vec<&str>) {
         let n = line[1..].parse::<u32>().unwrap();
 
         for _i in 0..n {
-            if dir == 'L' {pos -= 1}
-            if dir == 'R' {pos += 1}
-            if pos == -1 {pos = 99}
-            if pos == 100 {pos = 0}
+            if dir == 'L' {
+                pos -= 1
+            }
+            if dir == 'R' {
+                pos += 1
+            }
+            if pos == -1 {
+                pos = 99
+            }
+            if pos == 100 {
+                pos = 0
+            }
         }
-        if pos == 0 {answer += 1}
+        if pos == 0 {
+            answer += 1
+        }
     }
-
-    println!("Step 1 answer: {}", answer);
+    answer
 }
 
-fn step2(lines: Vec<&str>) {
+fn step2(lines: Vec<&str>) -> usize {
     let mut answer = 0;
     let mut pos = 50;
 
@@ -31,23 +41,36 @@ fn step2(lines: Vec<&str>) {
         let n = line[1..].parse::<u32>().unwrap();
 
         for _i in 0..n {
-            if dir == 'L' {pos -= 1}
-            if dir == 'R' {pos += 1}
-            if pos == -1 {pos = 99}
-            if pos == 100 {pos = 0}
-            if pos == 0 {answer += 1}
+            if dir == 'L' {
+                pos -= 1
+            }
+            if dir == 'R' {
+                pos += 1
+            }
+            if pos == -1 {
+                pos = 99
+            }
+            if pos == 100 {
+                pos = 0
+            }
+            if pos == 0 {
+                answer += 1
+            }
         }
-        
     }
-
-    println!("Step 2 answer: {}", answer);
+    answer
 }
 
 fn main() {
     let mut input = String::new();
-    io::stdin().read_to_string(&mut input).expect("Failed to read stdin");
+    io::stdin()
+        .read_to_string(&mut input)
+        .expect("Failed to read stdin");
     let lines: Vec<&str> = input.lines().collect();
 
-    step1(lines.clone());
-    step2(lines.clone());
+    let (step1_answer, step1_time) = timer::measure(|| step1(lines.clone()));
+    let (step2_answer, step2_time) = timer::measure(|| step2(lines.clone()));
+
+    println!("Step 1 answer: {}, in {:?}", step1_answer, step1_time);
+    println!("Step 2 answer: {}, in {:?}", step2_answer, step2_time);
 }
--- day-2-gift-shop/src/main.rs
+++ day-2-gift-shop/src/main.rs
@@ -1,13 +1,14 @@
 use std::io;
 use std::io::Read;
+use timer;
 
-fn step1(lines: Vec<&str>) {
+fn step1(lines: Vec<&str>) -> usize {
     let ranges = lines[0].split(",");
     let mut answer: usize = 0;
     for range in ranges {
         let parts: Vec<&str> = range.split("-").collect();
         if parts.len() != 2 {
-            return;
+            continue;
         }
         let first: usize = parts[0].parse::<usize>().unwrap();
         let last: usize = parts[1].parse::<usize>().unwrap();
@@ -15,12 +16,13 @@ fn step1(lines: Vec<&str>) {
             let str = i.to_string();
             if str.len() % 2 == 0 {
                 let (a, b) = str.split_at(str.len() / 2);
-                if a == b { answer += i };
+                if a == b {
+                    answer += i
+                };
             }
-            
         }
     }
-    println!("Step 1 answer: {}", answer)
+    return answer;
 }
 
 fn check_perm(str: String) -> bool {
@@ -34,28 +36,36 @@ fn check_perm(str: String) -> bool {
     return false;
 }
 
-fn step2(lines: Vec<&str>) {
+fn step2(lines: Vec<&str>) -> usize {
     let ranges = lines[0].split(",");
     let mut answer: usize = 0;
     for range in ranges {
         let parts: Vec<&str> = range.split("-").collect();
         if parts.len() != 2 {
-            return;
+            continue;
         }
         let first: usize = parts[0].parse::<usize>().unwrap();
         let last: usize = parts[1].parse::<usize>().unwrap();
         for i in first..last + 1 {
             let str = i.to_string();
-            if check_perm(str) { answer += i }
+            if check_perm(str) {
+                answer += i
+            }
         }
     }
-    println!("Step 2 answer: {}", answer)
+    return answer;
 }
 
 fn main() {
     let mut input = String::new();
-    io::stdin().read_to_string(&mut input).expect("Failed to read stdin");
+    io::stdin()
+        .read_to_string(&mut input)
+        .expect("Failed to read stdin");
     let lines: Vec<&str> = input.lines().collect();
-    step1(lines.clone());
-    step2(lines.clone());
+
+    let (step1_answer, step1_time) = timer::measure(|| step1(lines.clone()));
+    let (step2_answer, step2_time) = timer::measure(|| step2(lines.clone()));
+
+    println!("Step 1 answer: {}, in {:?}", step1_answer, step1_time);
+    println!("Step 2 answer: {}, in {:?}", step2_answer, step2_time);
 }
